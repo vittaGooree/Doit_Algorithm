832d31ff3d078085df9a2196a11b76e096dc03a6
이경준 <157299209+vittaGooree@users.noreply.github.com>
2024-02-27 14:45:42 +0900

    Create 연습문제_8.java

---
+++ 01_기본알고리즘/연습문제_8.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		// 6 4 > 456   1 4 > 1234
+		int a1 = 0;
+		int a2 = 0;
+		if (a > b) {
+			a1 = b;
+			a2 = a;
+		} else {
+			a1 = a;
+			a2 = b;
+		}
+		int ss = 0;
+		for (int i = a1; i <= a2; i++) {
+			ss += i;
+		}
+		
+		System.out.println(ss);
+		
+	}
+
+	
+}
